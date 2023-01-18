7c0851d92eddb64293dcae98c73f79b1278e50a6
Marcell Tanure <mtanure@waldale.com>
2023-01-18 11:38:47 -0400

    Managing unsubscriptions(declarative with takeUntil)

--- RxjsPatterns/src/app/rxjs-retrieve-list/rxjs-retrieve-list.component.ts
+++ RxjsPatterns/src/app/rxjs-retrieve-list/rxjs-retrieve-list.component.ts
@@ -1,28 +1,29 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { Recipe } from '../model/recipe';
 import { RxjsService } from '../rxjs.service';
 
 @Component({
   selector: 'app-rxjs-retrieve-list',
   templateUrl: './rxjs-retrieve-list.component.html',
-  styleUrls: ['./rxjs-retrieve-list.component.css']
+  styleUrls: ['./rxjs-retrieve-list.component.css'],
 })
 export class RxjsRetrieveListComponent implements OnInit, OnDestroy {
   recipes: Recipe[] = [];
-  subscription!: Subscription;
-
+  destroy$ = new Subject<void>();
   constructor(private rxjsService: RxjsService) {}
 
   ngOnInit(): void {
-    this. subscription = this.rxjsService.getRecipes()
-      .subscribe(result => {
-      this.recipes = result;
-    });
+    this.rxjsService
+      .getRecipes()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((result) => {
+        this.recipes = result;
+      });
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.destroy$.next();
+    this.destroy$.complete();
   }
-
 }
