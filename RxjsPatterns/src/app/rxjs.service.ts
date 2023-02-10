462a813397e8b1093d162b75dd6be182aebc8c01
Marcell Tanure <mtanure@waldale.com>
2023-02-09 21:39:11 -0400

    Cache with shareReplay implemented

--- RxjsPatterns/src/app/rxjs.service.ts
+++ RxjsPatterns/src/app/rxjs.service.ts
@@ -2,15 +2,13 @@ import { Injectable } from '@angular/core';
 import { Recipe } from './model/recipe';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { BehaviorSubject, catchError, Observable, of } from 'rxjs';
+import { BehaviorSubject, catchError, Observable, of, switchMap, timer, pipe, shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RxjsService {
-  recipes$ = this.http.get<Recipe[]>(`${environment.basePath}/recipes`).pipe(
-    catchError(error => of([]))
-  );
+  recipes$ = this.getRecipesList();
 
   /* Create the action stream */
   private filterRecipeSubject = new BehaviorSubject<Recipe>({title: '', category: ''});
@@ -26,5 +24,23 @@ export class RxjsService {
 
   saveRecipe(formValue: Recipe): Observable<Recipe> {
     return this.http.post<Recipe>(`${environment.basePath}/recipes/save`, formValue);
+  }
+
+  getRecipesList(): Observable<Recipe[]> {
+    if (!this.recipes$) {
+      return timer(0, 50000).pipe(
+        switchMap(_ => this.http.get<Recipe[]>(`${environment.basePath}/recipes`)),
+        /* Popular way of doing it */
+        shareReplay(1)
+        /* Recommended way with RxJS7
+        share({
+          connector : () => new ReplaySubject(),
+          resetOnRefCountZero : true,
+          restOnComplete: true,
+          resetOnError: true
+        }) */
+      );
+    }
+    return this.recipes$;
   }
 }
